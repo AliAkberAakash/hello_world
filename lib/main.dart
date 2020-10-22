989ff6f48eea8f35ef18c61cc7f41abe98678ac9
aliakberaakash <ali852609@gmail.com>
2020-10-23 00:22:36 +0600

    added color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/di/dependency_injection.dart';
 import 'package:hello_world/ui/features/home/home_page.dart';
+import 'package:hello_world/utils/my_colors.dart';
 
 void main() {
   setup();
@@ -12,10 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hello World',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: primary,
+        primaryColorDark: primary_dark,
+        accentColor: accent,
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
       ),
       home: HomePage(),
     );
---
+++ lib/utils/my_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/rendering.dart';
+
+const white = Color(0xffffffff);
+const black = Color(0xff000000);
+const primary = Color(0xffffe2e2);
+const primary_dark = Color(0xffffc7c7);
+const accent = Color(0xffaaaaaa);
